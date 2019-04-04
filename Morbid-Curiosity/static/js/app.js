0c8b1ab8ac09e9cd641cb5a78ae5b9b474379fb2
SarahFawson <sarahelizabethfawson@gmail.com>
2019-04-03 23:31:09 -0400

    html/css updates + trying sticky navbar

--- Morbid-Curiosity/static/js/app.js
+++ Morbid-Curiosity/static/js/app.js
@@ -1,7 +1,23 @@
 // ###############################
-// GET METADATA
+// STICKY NAVBAR
 // ###############################
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function {stickyNavbar()};
 
+// Get the navbar
+var navbar = document.getElementById('navbar');
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNavbar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
 
 // ###############################
 // BUILD USA HEAT MAP WITH LEAFLET
@@ -47,6 +63,7 @@ function buildRadar() {
     options: {
       responsive: true,
       maintainAspectRadio: true,
+      scale: hide=false,
       tooltips: {
         enable: true,
         mode: 'label'
