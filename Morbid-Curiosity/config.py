9ecfce49b9a3e12006ca22e429a61e3e76423b86
Sarah Fawson <42820041+sarahfawson@users.noreply.github.com>
2019-04-03 23:39:01 -0400

    Delete config.py

--- Morbid-Curiosity/config.py
+++
@@ -1,5 +0,0 @@
-# twitter
-consumer_key = "lpQ2uGm1MbDppmpPrReuo6f5y"
-consumer_secret = "eHxtheCaFGoH6nPzGBVtYDvAPmDmsdBdxNJm1YCuXJfFFsUO84"
-access_token = "4176875298-0Md2Spx142YO1gu7XZTDFFnC5VAJcsiwniRdtko"
-access_token_secret = "fgIWtVEYYWXtOuDrXIbMFrzPo68Dk5O0rDDP0B2eKosPh"
