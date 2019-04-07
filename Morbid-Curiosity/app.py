ad09bd3fd68d6236d8397e06f5433b8222ce21fc
SarahFawson <sarahelizabethfawson@gmail.com>
2019-04-07 10:24:30 -0400

    added sql code, formatting, process page

--- Morbid-Curiosity/app.py
+++ Morbid-Curiosity/app.py
@@ -11,6 +11,9 @@ from sqlalchemy import create_engine
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+from config import remote_db_endpoint, remote_db_port
+from config import remote_morbid_dbname, remote_morbid_dbuser, remote_morbid_dbpwd
+
 import tweets
 
 app = Flask(__name__)
@@ -19,47 +22,63 @@ app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/gendermortality.sqlite"
-# db = SQLAlchemy(app)
+pymysql.install_as_MySQLdb()
 
-# # reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(db.engine, reflect=True)
-
-# # Save references to each table
-# genderData = Base.classes.cleangenderdeaths
-
+engine = create_engine(f"mysql://{remote_morbid_dbuser}:{remote_morbid_dbpwd}@{remote_db_endpoint}:{remote_db_port}/{remote_morbid_dbname}")
+conn = engine.connect()
 
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
 
-@app.route("/about")
+@app.route("/process")
 def about():
-    """Return the about us page."""
-    # perhaps I could use index and replace body content
-    return render_template("about.html")
+    """Return the detail page."""
+    # this page is where we explain the project process
+    return render_template("detail.html")
 
-@app.route("/mortality")
-def mortality():
-    """Return mortality data"""
-    # code here
+
+@app.route("/denseData")
+def dense():
+    """Return density data"""
+    data_df = pd.read_sql("SELECT * FROM density", conn)
+    df = pd.DataFrame(data_df, columns=['Density', 'Percent', 'Cause of Death', 'Deaths', 'Population', 'Rate per 100k'])
+    return jsonify(df.to_dict(orient="records"))
+
+
+@app.route("/genderData")
+def gender():
+    """Return gender data"""
+
+    data_df = pd.read_sql("SELECT * FROM gender", conn)
+    df = pd.DataFrame(data_df, columns=['Gender', 'Cause of Death', 'Deaths', 'Population', 'Rate per 1000', 'Percent'])
+    return jsonify(df.to_dict(orient="records"))
+
+
+@app.route("/sviData")
+def sviData():
+    """Return svi and life expectancy data"""
+
+    data_df = pd.read_sql("SELECT * FROM sviLife", conn)
+    df = pd.DataFrame(data_df, columns=["FIPS","Location","Life Expectancy","RPL_THEMES","RPL_THEME1","RPL_THEME2","RPL_THEME3","RPL_THEME4"])
+    return jsonify(df.to_dict(orient="records"))
+
 
 @app.route("/tweets")
 def cdctweets():
     """Return CDC twitter data"""
     return jsonify(tweets)
 
-# do we need this
-@app.route("/deathsbygender")
-def gender():
-    """Return summarized deaths by gender data"""
-    df = pd.read_csv("../Datasets/cleangenderdeath.csv")
-    return jsonify(list(df.columns)[2:])
-
-
 
 if __name__ == "__main__":
     app.run()
+
+
+
+
+
+## Screenshot of code
+
+
+
--- Morbid-Curiosity/static/js/app.js
+++ Morbid-Curiosity/static/js/app.js
@@ -2,22 +2,22 @@
 // STICKY NAVBAR
 // ###############################
 // When the user scrolls the page, execute myFunction 
-window.onscroll = function {stickyNavbar()};
+// window.onscroll = function stickyNavbar();
 
-// Get the navbar
-var navbar = document.getElementById('navbar');
+// // Get the navbar
+// var navbar = document.getElementById('navbar');
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickyNavbar() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-};
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function stickyNavbar() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// };
 
 // ###############################
 // BUILD USA HEAT MAP WITH LEAFLET
