a0260a11427d2ddc99ac420c180be2c542d93ece
SarahFawson <sarahelizabethfawson@gmail.com>
2019-04-07 23:01:49 -0400

    pulling in data to app.js, wip

--- Morbid-Curiosity/app.py
+++ Morbid-Curiosity/app.py
@@ -15,10 +15,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import remote_db_endpoint, remote_db_port
 from config import remote_morbid_dbname, remote_morbid_dbuser, remote_morbid_dbpwd
 
-import tweets
-
 app = Flask(__name__)
-
 
 #################################################
 # Database Setup
@@ -34,51 +31,41 @@ def index():
 
 @app.route("/process")
 def about():
-    """Return the detail page."""
-    # this page is where we explain the project process
+    """Return the detail page to explain the project process."""
     return render_template("detail.html")
 
+# @app.route("/denseData")
+# def dense():
+#     """Return density data"""
+#     conn = engine.connect()
+#     data_df = pd.read_sql("SELECT * FROM density", conn)
+#     df = pd.DataFrame(data_df, columns=['Density', 'Percent', 'Cause of Death', 'Deaths', 'Population', 'Rate per 100k'])
+#     df.rename(columns = {'Cause of Death': 'Cause_of_Death', 'Rate per 100k': 'Rate_per_100k'}, inplace = True)
 
-@app.route("/denseData")
-def dense():
-    """Return density data"""
-    data_df = pd.read_sql("SELECT * FROM density", conn)
-    df = pd.DataFrame(data_df, columns=['Density', 'Percent', 'Cause of Death', 'Deaths', 'Population', 'Rate per 100k'])
-    return jsonify(df.to_dict(orient="records"))
-
+#     conn.close()
+#     return jsonify(df.to_dict(orient="records"))
 
 @app.route("/genderData")
 def gender():
     """Return gender data"""
-
+    conn = engine.connect()
     data_df = pd.read_sql("SELECT * FROM gender", conn)
     df = pd.DataFrame(data_df, columns=['Gender', 'Cause of Death', 'Deaths', 'Population', 'Rate per 1000', 'Percent'])
+    df.rename(columns = {'Cause of Death': 'Cause_of_Death', 'Rate per 1000': 'Rate_per_1000'}, inplace = True)
+    
+    conn.close()
     return jsonify(df.to_dict(orient="records"))
 
+# @app.route("/sviData")
+# def sviData():
+#     """Return svi and life expectancy data"""
+#     conn = engine.connect()
+#     data_df = pd.read_sql("SELECT * FROM sviLife", conn)
+#     df = pd.DataFrame(data_df, columns=['FIPS','Location','Life Expectancy','RPL_THEMES','RPL_THEME1','RPL_THEME2','RPL_THEME3','RPL_THEME4'])
+#     df.rename(columns = {'Life Expectancy': 'Life_Expectancy'}, inplace = True)
 
-@app.route("/sviData")
-def sviData():
-    """Return svi and life expectancy data"""
-
-    data_df = pd.read_sql("SELECT * FROM sviLife", conn)
-    df = pd.DataFrame(data_df, columns=["FIPS","Location","Life Expectancy","RPL_THEMES","RPL_THEME1","RPL_THEME2","RPL_THEME3","RPL_THEME4"])
-    return jsonify(df.to_dict(orient="records"))
-
-
-@app.route("/tweets")
-def cdctweets():
-    """Return CDC twitter data"""
-    return jsonify(tweets)
-
+#     conn.close()
+#     return jsonify(df.to_dict(orient="records"))
 
 if __name__ == "__main__":
-    app.run()
-
-
-
-
-
-## Screenshot of code
-
-
-
+    app.run(debug=True)
--- Morbid-Curiosity/static/js/app.js
+++ Morbid-Curiosity/static/js/app.js
@@ -22,56 +22,68 @@
 // ###############################
 // BUILD RADAR CHART WITH CHARTS.JS
 // ###############################
-function buildRadar() {
+
 // ####################
 // radar numero 1
 // ####################
-  var ctx = document.getElementById('radar1').getContext('2d');
-  var radarChart1 = new Chart(ctx, {
-    type: 'radar',
+function buildRadar1() {
 
-    data: {
-      labels: ['Cancer','Heart Disease', 'Stroke', 'Respiratory', 'Accidents'],
-      datasets: [{
+  var url = `/genderData`;
+  d3.json(url).then((data) => {
+
+    var diseaseType = Object.values(data.Cause_of_Death);
+    console.log(diseaseType);
+    var deathRate = Object.values(data.Percent);
+    console.log(Percent);
+    var gender = Object.values(data.Gender);
+    console.log(gender);
+
+    // select id to place chart
+    var ctx = document.getElementById('radar1').getContext('2d');
+    // build chart
+    var radarChart1 = new Chart(ctx, {
+      type: 'radar',
+      data: {
+        labels: diseaseType,
+        datasets: [{
           label: 'Males',
           backgroundColor: 'rgba(18,94,227,.5)',
           borderColor: 'rgba(18,94,227, .5)',
           pointBackgroundColor:'rgba(18,94,227, .5)', 
           pointRadius: 1,
-          data: [22.46561055745661, 24.229162974374354, 4.238978791472963, 5.2165841865368785, 7.417644840106701]
-      }, {
-          label: 'Femles',
+          data: deathRate,
+        }, {
+          label: 'Females',
           backgroundColor: 'rgba(72, 209, 204,.5)',
           borderColor: 'rgba(72, 209, 204,.5)',
           pointBackgroundColor:'rgba(72, 209, 204,.5)',
           pointRadius: 1,
           data: [21.091105716123568, 22.02314474068052, 6.1596843077877015, 6.067652167332496, 4.2790062970001665]
-        }
-      ]
-    },
-
-    options: {
-      responsive: true,
-      maintainAspectRadio: true,
-      scale: hide=false,
-      tooltips: {
-        enable: true,
-        mode: 'label'
+          }
+        ]
       },
-      legend: {
-        display: true,
-        position: 'bottom',
-        labels: {
-          fontColor: 'rgb(0, 0, 0)'
-        }
-      },
-      title: {
-        display: true,
-        text: 'Female vs. Male',
-        fontSize: 16
-      },
-      tooltips: {
-        callbacks: {
+      options: {
+        responsive: true,
+        maintainAspectRadio: true,
+        scale: hide=false,
+        tooltips: {
+          enable: true,
+          mode: 'label'
+        },
+        legend: {
+          display: true,
+          position: 'bottom',
+          labels: {
+            fontColor: 'rgb(0, 0, 0)'
+          }
+        },
+        title: {
+          display: true,
+          text: 'Female vs. Male',
+          fontSize: 16
+        },
+        tooltips: {
+          callbacks: {
             label: function(tooltipItem, data) {
                 var label = data.datasets[tooltipItem.datasetIndex].label || '';
 
@@ -81,136 +93,154 @@ function buildRadar() {
                 label += Math.round(tooltipItem.yLabel * 100) / 100;
                 return label;
             }
+          }
         }
-    }
-  }
-});
+      }
+    });
+  });
+};
 
 // ####################
 // radar numero 2 
 // ####################
-var ctx = document.getElementById('radar2').getContext('2d');
-var radarChart1 = new Chart(ctx, {
-  type: 'radar',
+// function buildRadar2() {
+//   var url = `/denseData`;
+//   d3.json(url).then((data) => {
+//     console.log(data)
 
-  data: {
-    labels: ['Cancer','Heart Disease', 'Stroke', 'Respiratory', 'Accidents'],
-    datasets: [{
-        label: 'Urban',
-        backgroundColor: 'rgba(235, 45, 31,.5)',
-        borderColor: 'rgba(235, 45, 31,.5)',
-        pointBackgroundColor:'rgba(235, 45, 31,.5)',
-        pointRadius: 1,
-        data: [22, 7, 19, 6, 21]
-    }, {
-        label: 'Rural',
-        backgroundColor: 'rgba(72, 209, 204,.5)',
-        borderColor: 'rgba(72, 209, 204,.5)',
-        pointBackgroundColor: 'rgba(72, 209, 204,.5)',
-        pointRadius: 1,
-        data: [21, 11, 6, 9, 4]
-      }
-    ]
-  },
+//     var diseaseType = Object.values(data.Cause_of_Death);
+//     var deathRate = Object.values(data.Percent);
+//     var densityType = Object.values(data.Percent);
 
-  options: {
-    responsive: true,
-    maintainAspectRadio: true,
-    tooltips: {
-      enable: true,
-      mode: 'label'
-    },
-    legend: {
-      display: true,
-      position: 'bottom',
-      labels: {
-        fontColor: 'rgb(0, 0, 0)'
-      }
-    },
-    title: {
-      display: true,
-      text: 'Urban vs Rural Living',
-      fontSize: 16
-    },
-    tooltips: {
-      callbacks: {
-          label: function(tooltipItem, data) {
-              var label = data.datasets[tooltipItem.datasetIndex].label || '';
-
-              if (label) {
-                  label += ': ';
-              }
-              label += Math.round(tooltipItem.yLabel * 100) / 100;
-              return label;
-          }
-      }
-  }
-}
-});
+//     var ctx = document.getElementById('radar2').getContext('2d');
+//     var radarChart1 = new Chart(ctx, {
+//       type: 'radar',
+//       data: {
+//         labels: ['Cancer','Heart Disease', 'Stroke', 'Respiratory', 'Accidents'],
+//         datasets: [{
+//           label: 'Urban',
+//           backgroundColor: 'rgba(235, 45, 31,.5)',
+//           borderColor: 'rgba(235, 45, 31,.5)',
+//           pointBackgroundColor:'rgba(235, 45, 31,.5)',
+//           pointRadius: 1,
+//           data: [22, 7, 19, 6, 21]
+//         }, {
+//           label: 'Rural',
+//           backgroundColor: 'rgba(72, 209, 204,.5)',
+//           borderColor: 'rgba(72, 209, 204,.5)',
+//           pointBackgroundColor: 'rgba(72, 209, 204,.5)',
+//           pointRadius: 1,
+//           data: [21, 11, 6, 9, 4]
+//           }
+//         ]
+//       },
+//       options: {
+//         responsive: true,
+//         maintainAspectRadio: true,
+//         tooltips: {
+//           enable: true,
+//           mode: 'label'
+//         },
+//         legend: {
+//           display: true,
+//           position: 'bottom',
+//           labels: {
+//             fontColor: 'rgb(0, 0, 0)'
+//           }
+//         },
+//         title: {
+//           display: true,
+//           text: 'Urban vs Rural Living',
+//           fontSize: 16
+//         },
+//         tooltips: {
+//           callbacks: {
+//             label: function(tooltipItem, data) {
+//               var label = data.datasets[tooltipItem.datasetIndex].label || '';
+//               if (label) {
+//                   label += ': ';
+//               }
+//               label += Math.round(tooltipItem.yLabel * 100) / 100;
+//               return label;
+//             }
+//           }
+//         }
+//       }
+//     });
+//   });
+// };
 
 // ####################
 // radar numero 3
 // ####################
-var ctx = document.getElementById('radar3').getContext('2d');
-var radarChart1 = new Chart(ctx, {
-  type: 'radar',
+// function buildRadar3() {
 
-  data: {
-    labels: ['Cancer','Heart Disease', 'Stroke', 'Respiratory', 'Accidents'],
-    datasets: [{
-        label: 'In Poverty',
-        backgroundColor: 'rgba(105,105,105,.5)',
-        borderColor: 'rgba(105,105,105, .5)',
-        pointBackgroundColor:'rgba(105,105,105, .5)',
-        pointRadius: 1,
-        data: [33, 24, 11, 22, 9]
-    }, {
-        label: 'Not in Poverty',
-        backgroundColor: 'rgba(235, 45, 31,.5)',
-        borderColor: 'rgba(235, 45, 31,.5)',
-        pointBackgroundColor: 'rgba(235, 45, 31,.5)',
-        pointRadius: 1,
-        data: [5, 6, 6, 4, 3]
-      }
-    ]
-  },
+//   var url = `/sviData`;
+//   d3.json(url).then((data) => {
+//     console.log(data)
 
-  // Configuration options go here
-  options: {
-    responsive: true,
-    maintainAspectRadio: true,
-    tooltips: {
-      enable: true,
-      mode: 'label'
-    },
-    legend: {
-      display: true,
-      position: 'bottom',
-      labels: {
-        fontColor: 'rgb(0, 0, 0)'
-      }
-    },
-    title: {
-      display: true,
-      text: 'Above vs. Below the Poverty Line',
-      fontSize: 16
-    },
-    tooltips: {
-      callbacks: {
-          label: function(tooltipItem, data) {
-              var label = data.datasets[tooltipItem.datasetIndex].label || '';
+//     var diseaseType = Object.values(data.Cause_of_Death);
+//     var deathRate = Object.values(data.Percent);
 
-              if (label) {
-                  label += ': ';
-              }
-              label += Math.round(tooltipItem.yLabel * 100) / 100;
-              return label;
-          }
-      }
-  }
-}
-});
+//     var ctx = document.getElementById('radar3').getContext('2d');
+//     var radarChart1 = new Chart(ctx, {
+//       type: 'radar',
+//       data: {
+//         labels: ['Cancer','Heart Disease', 'Stroke', 'Respiratory', 'Accidents'],
+//         datasets: [{
+//           label: 'In Poverty',
+//           backgroundColor: 'rgba(105,105,105,.5)',
+//           borderColor: 'rgba(105,105,105, .5)',
+//           pointBackgroundColor:'rgba(105,105,105, .5)',
+//           pointRadius: 1,
+//           data: [33, 24, 11, 22, 9]
+//         }, {
+//           label: 'Not in Poverty',
+//           backgroundColor: 'rgba(235, 45, 31,.5)',
+//           borderColor: 'rgba(235, 45, 31,.5)',
+//           pointBackgroundColor: 'rgba(235, 45, 31,.5)',
+//           pointRadius: 1,
+//           data: [5, 6, 6, 4, 3]
+//           }
+//         ]
+//       },
+//       options: {
+//         responsive: true,
+//         maintainAspectRadio: true,
+//         tooltips: {
+//           enable: true,
+//           mode: 'label'
+//         },
+//         legend: {
+//           display: true,
+//           position: 'bottom',
+//           labels: {
+//             fontColor: 'rgb(0, 0, 0)'
+//           }
+//         },
+//         title: {
+//           display: true,
+//           text: 'Above vs. Below the Poverty Line',
+//           fontSize: 16
+//         },
+//         tooltips: {
+//           callbacks: {
+//             label: function(tooltipItem, data) {
+//               var label = data.datasets[tooltipItem.datasetIndex].label || '';
 
-}
+//               if (label) {
+//                   label += ': ';
+//               }
+//               label += Math.round(tooltipItem.yLabel * 100) / 100;
+//               return label;
+//             }
+//           }
+//         }
+//       }
+//     });
+//   });
+// }
 
-buildRadar();
+buildRadar1();
+// buildRadar2();
+// buildRadar3();
