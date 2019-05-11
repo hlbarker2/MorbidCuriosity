19cbbe12ab86f0707fa1f17745a6898883a8ebde
Connor Vergara <44386095+cvergara93@users.noreply.github.com>
2019-05-11 10:33:08 -0400

    Delete config.py

--- Morbid-Curiosity/config.py
+++
@@ -1,9 +0,0 @@
-# AWS Database Info
-remote_db_endpoint = "morbid.cu97gshsimlg.us-east-1.rds.amazonaws.com"
-remote_db_port = 3306
-remote_morbid_dbname = "morbid"
-remote_morbid_dbuser = "root"
-remote_morbid_dbpwd = "PlayNice345"
-
-#MapBox
-API_KEY = "sk.eyJ1IjoiY3ZlcmdhcmE5MyIsImEiOiJjanRsbzVkdnQwbnh0NDRrNjR6cmlnOHJsIn0.0byOExQ90bOcfkwwj7yMfA"
