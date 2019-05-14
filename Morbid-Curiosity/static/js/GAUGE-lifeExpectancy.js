d7eb1ce2a86a8f071372f058d78e1de150e75c4b
Hannah Barker <hlbarker2@gmail.com>
2019-05-14 18:34:58 -0400

    Add files via upload

--- Morbid-Curiosity/static/js/GAUGE-lifeExpectancy.js
+++ Morbid-Curiosity/static/js/GAUGE-lifeExpectancy.js
@@ -2,31 +2,22 @@ var g2
 function buildLifeExpectancyGauge(life) {
 
   if (!g2) {
-
+    
     g2 = new JustGage({
-      id: "life-gauge",
+      id: 'life-gauge',
       value: life,
       min: 60,
       max: 90,
-      symbol: " yrs",
-      pointer: true,
       decimals: 2,
+      symbol: ' yrs',
+      pointer: false,
       label: "Life Expectancy",
-      pointerOptions: {
-        toplength: -15,
-        bottomlength: 10,
-        bottomwidth: 5,
-        color: "#EB2D1F",
-        stroke: "#ffffff",
-        stroke_width: 1,
-        stroke_linecap: "round"
-      },
       gaugeWidthScale: 0.6,
       counter: true,
-      relativeGaugeSize: true
+      relativeGaugeSize: true,
+      levelColors: ["#eb2d1f"]
     });
+  }
+  else g2.refresh(life);
 
-  } else g2.refresh(life);
-
-    };
-
+};
